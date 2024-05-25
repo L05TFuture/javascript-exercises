850bfface994d39c650c562b9d4e01c933090d74
L05TFuture <keithsu6705@gmail.com>
2024-05-25 22:28:45 +0800

    uh

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let newstring = ''
+    if (num<0){return 'ERROR'}
+    else{
+for( let i=1; i<=num; i++)
+    {newstring +=`${str}`}
+return newstring}
 };
 
 // Do not edit below this line
